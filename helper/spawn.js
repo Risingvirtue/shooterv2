497030dae8631b92f9aa826054f75cfe6ba77e7e
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-30 01:00:49 -0800

    socket

---
+++ helper/spawn.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	spawn: function() {
+		let x = Math.random();
+		let y = Math.random();
+		return {x: x, y: y};
+	}
+};
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var server = app.listen('3000');
 var socket = require('socket.io');
 var io = socket(server);
+
+var spawn = require('./helper/spawn.js');
 io.sockets.on('connection', newConnection);
 
 app('/', function(req, res) {
@@ -12,6 +14,6 @@ app('/', function(req, res) {
 
 function newConnection(socket) {
 	console.log('New Connection: ' + socket.id);
-	
+	console.log(spawn.spawn());
 	
 }
